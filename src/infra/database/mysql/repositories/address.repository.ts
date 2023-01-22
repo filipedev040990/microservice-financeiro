b1b8ea503dd55f989e5898ea19f3c7b33fdac008
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-22 17:08:15 -0300

    feat: add address repository save method

---
+++ src/infra/database/mysql/repositories/address.repository.ts
@@ -0,0 +1,19 @@
+import { AddressInput, SaveAddressRepositoryInterface } from '@/domain/interfaces'
+import { prismaClient } from './prisma-client'
+
+export class AddressRepository implements SaveAddressRepositoryInterface {
+  async save (input: AddressInput): Promise<void> {
+    await prismaClient.address.create({
+      data: {
+        client_id: input.client_id,
+        cep: input.cep,
+        street: input.street,
+        number: input.number,
+        complement: input.complement,
+        district: input.district,
+        city: input.city,
+        state: input.state
+      }
+    })
+  }
+}
