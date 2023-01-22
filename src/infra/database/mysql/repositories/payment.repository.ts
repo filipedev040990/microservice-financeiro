81a7304953bf3363b17a9468a2b78597b76e6100
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-22 17:33:32 -0300

    feat: add payment repository

---
+++ src/infra/database/mysql/repositories/payment.repository.ts
@@ -0,0 +1,17 @@
+import { PaymentInput, SavePaymentRepositoryInterface } from '@/domain/interfaces'
+import { prismaClient } from './prisma-client'
+
+export class PaymentRepository implements SavePaymentRepositoryInterface {
+  async save (input: PaymentInput): Promise<void> {
+    await prismaClient.payment.create({
+      data: {
+        client_id: input.client_id,
+        status: input.status,
+        attempts_processing: input.attempts_processing,
+        value: input.value,
+        installments: input.installments,
+        description: input.description
+      }
+    })
+  }
+}
