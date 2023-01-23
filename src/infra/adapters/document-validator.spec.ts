ac55f315e09b3b07fd0fe538eedf05f4eff1a85a
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-23 19:12:29 -0300

    feat: ensure Document Validator return false if validation fails with cnpj

--- src/infra/adapters/document-validator.spec.ts
+++ src/infra/adapters/document-validator.spec.ts
@@ -14,8 +14,14 @@ describe('Card Validator Adapter', () => {
     const sut = makeSut()
     expect(sut.execute('pf', '123456')).toBe(false)
   })
+
   test('should return true if document validator returns true', () => {
     const sut = makeSut()
     expect(sut.execute('pj', '12738608000105')).toBe(true)
+  })
+
+  test('should return true if document validator returns true', () => {
+    const sut = makeSut()
+    expect(sut.execute('pj', 'x132x13x2123x1x')).toBe(false)
   })
 })
