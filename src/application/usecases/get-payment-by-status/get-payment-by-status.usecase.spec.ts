a90496b9658224d3ccaf3d2d5551336def6760d6
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-24 07:02:01 -0300

    feat: ensure GetPaymentByStatusUseCase return null if PaymentRepository.getByStatus returns null

--- src/application/usecases/get-payment-by-status/get-payment-by-status.usecase.spec.ts
+++ src/application/usecases/get-payment-by-status/get-payment-by-status.usecase.spec.ts
@@ -1,8 +1,19 @@
+import { Payment } from '@/domain/entities/payment'
 import { GetPaymentByStatusRepositoryInterface } from '@/domain/repositories/get-payment-by-status-repository.interface'
 import { GetPaymentByStatusUseCase } from './get-payment-by-status.usecase'
 
+const makePayment = (): Payment => ({
+  id: 'any id',
+  client_id: '123456789',
+  status: 'waiting',
+  attempts_processing: 0,
+  description: 'Compra de curso',
+  installments: 12,
+  value: 1200
+})
+
 const paymentRepository: jest.Mocked<GetPaymentByStatusRepositoryInterface> = {
-  getByStatus: jest.fn()
+  getByStatus: jest.fn().mockResolvedValue(makePayment())
 }
 
 const makeSut = (): GetPaymentByStatusUseCase => {
@@ -17,16 +28,20 @@ describe('GetPaymentByStatusUseCase', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
-
   test('should call PaymentRepository.getByStatus once and with correct values', async () => {
     await sut.execute('waiting')
     expect(paymentRepository.getByStatus).toHaveBeenCalledTimes(1)
     expect(paymentRepository.getByStatus).toHaveBeenCalledWith('waiting')
   })
 
-  test('should return null if PaymentRepository.getByStatus returns null', async () => {
+  test('should return an payment on success', async () => {
     const response = await sut.execute('waiting')
+    expect(response).toBeTruthy()
+    expect(response).toEqual(makePayment())
+  })
+
+  test('should return null if PaymentRepository.getByStatus returns null', async () => {
     paymentRepository.getByStatus.mockReturnValueOnce(null)
-    expect(response).toBeNull()
+    expect(await sut.execute('waiting')).toBeNull()
   })
 })
