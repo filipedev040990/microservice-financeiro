fcd5b9d444ef1180c605a2e11d8f09f2ff6a23d8
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-30 08:44:39 -0300

    feat: ensure CardValidator return 400 if CardValidatorAdapter returns false

--- src/infra/validators/card-validator.ts
+++ src/infra/validators/card-validator.ts
@@ -1,10 +1,13 @@
 import { ValidationInterface } from '@/domain/validation/validation.interface'
+import { InvalidParamError } from '@/shared/errors'
 import { CardValidatorAdapter } from '../adapters/card-validator.adapter'
 
 export class CardValidator implements ValidationInterface {
   constructor (private readonly cardValidatorAdapter: CardValidatorAdapter) {}
   validate (input: any): Error {
-    this.cardValidatorAdapter.execute(input.card)
-    return null
+    const cardValid = this.cardValidatorAdapter.execute(input.card)
+    if (!cardValid) {
+      return new InvalidParamError('card')
+    }
   }
 }
--- tests/infra/card-validator.spec.ts
+++ tests/infra/card-validator.spec.ts
@@ -1,5 +1,6 @@
 import { CardValidatorInterface } from '@/domain/validation/card-validator.interface'
 import { CardValidator } from '@/infra/validators/card-validator'
+import { InvalidParamError } from '@/shared/errors'
 
 const cardValidatorAdapter: jest.Mocked<CardValidatorInterface> = {
   execute: jest.fn().mockReturnValue(true)
@@ -22,5 +23,10 @@ describe('CardValidator', () => {
   test('should call CardValidatorAdapter with correct card', () => {
     sut.validate({ card: '123456789' })
     expect(cardValidatorAdapter.execute).toHaveBeenCalledWith('123456789')
+  })
+
+  test('should return 400 if CardValidatorAdapter return false', () => {
+    cardValidatorAdapter.execute.mockReturnValueOnce(false)
+    expect(sut.validate({ card: 'invalidCard13132123' })).toEqual(new InvalidParamError('card'))
   })
 })
