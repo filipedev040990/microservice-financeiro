a87b4afa89f4070eb73b774e632d46e0b78e11d1
Filipe Siqueira <filipedev040990@gmail.com>
2023-01-30 08:24:02 -0300

    feat: ensure DocumentValidator not return if DocumentValidatorAdapter return true

--- tests/infra/document-validator.spec.ts
+++ tests/infra/document-validator.spec.ts
@@ -3,7 +3,7 @@ import { DocumentValidatorAdapter } from '@/infra/adapters/document-validator'
 import { InvalidParamError } from '@/shared/errors'
 
 const documentValidatorAdapterStub: jest.Mocked<DocumentValidatorAdapter> = {
-  execute: jest.fn()
+  execute: jest.fn().mockReturnValue(true)
 }
 
 const makeSut = (): DocumentValidator => {
@@ -16,6 +16,9 @@ describe('DocumentValidator', () => {
   beforeEach(() => {
     sut = makeSut()
   })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
   test('should call DocumentValidator with correct values', () => {
     sut.validate({ document: '123456789' })
     expect(documentValidatorAdapterStub.execute).toHaveBeenCalledWith('cpf', '123456789')
@@ -24,5 +27,9 @@ describe('DocumentValidator', () => {
   test('should return Invalid Param Error if validation fails', () => {
     documentValidatorAdapterStub.execute.mockReturnValueOnce(false)
     expect(sut.validate({ document: '123456789' })).toEqual(new InvalidParamError('cpf'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    expect(sut.validate({ document: '123456789' })).toBeFalsy()
   })
 })
